07edd864b22949ac3f9b0e70bd336f8886e908e0
weeraphon-benz <bnzpogo@gmail.com>
2025-06-04 17:44:14 +0700

    Add files via upload

--- fesm2022/connex-ui.mjs
+++ fesm2022/connex-ui.mjs
@@ -1,5 +1,17 @@
 import * as i0 from '@angular/core';
-import { NgModule, Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+
+class ConnexButtonComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: ConnexButtonComponent, deps: [], target: i0.ɵɵFactoryTarget.Component }); }
+    static { this.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "16.2.12", type: ConnexButtonComponent, selector: "connex-button", ngImport: i0, template: "<p>\n  connex-button works!\n</p>\n", styles: [""] }); }
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: ConnexButtonComponent, decorators: [{
+            type: Component,
+            args: [{ selector: 'connex-button', template: "<p>\n  connex-button works!\n</p>\n" }]
+        }], ctorParameters: function () { return []; } });
 
 class ConnexUiModule {
     static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: ConnexUiModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule }); }
@@ -18,18 +30,6 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.12", ngImpo
                     ]
                 }]
         }] });
-
-class ConnexButtonComponent {
-    constructor() { }
-    ngOnInit() {
-    }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: ConnexButtonComponent, deps: [], target: i0.ɵɵFactoryTarget.Component }); }
-    static { this.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "16.2.12", type: ConnexButtonComponent, selector: "connex-button", ngImport: i0, template: "<p>\n  connex-button works!\n</p>\n", styles: [""] }); }
-}
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: ConnexButtonComponent, decorators: [{
-            type: Component,
-            args: [{ selector: 'connex-button', template: "<p>\n  connex-button works!\n</p>\n" }]
-        }], ctorParameters: function () { return []; } });
 
 /*
  * Public API Surface of connex-ui
